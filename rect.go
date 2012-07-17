b0c3449045258bbdb716de757bc03757e8ae2f53
paul <paul@Thor-dlan.fritz.box>
2012-07-17 15:50:29 +0200

    license + fix

--- rect.go
+++ rect.go
@@ -1,3 +1,15 @@
+/*
+Copyright (c) 2012 krepa098 (krepa098 at gmail dot com)
+This software is provided 'as-is', without any express or implied warranty.
+In no event will the authors be held liable for any damages arising from the use of this software.
+Permission is granted to anyone to use this software for any purpose, including commercial applications, 
+and to alter it and redistribute it freely, subject to the following restrictions:
+	1.	The origin of this software must not be misrepresented; you must not claim that you wrote the original software. 
+		If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+	2. 	Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+	3. 	This notice may not be removed or altered from any source distribution.
+*/
+
 package GoSFML2
 
 // #include <SFML/Graphics.h>
@@ -26,20 +38,20 @@ type Recti struct {
 ///		FUNCS
 /////////////////////////////////////
 
-func (this *Rectf) Contains(x, y float32) bool {
+func (this Rectf) Contains(x, y float32) bool {
 	return sfBool2Go(C.sfFloatRect_contains(this.toCPtr(), C.float(x), C.float(y)))
 }
 
-func (this *Recti) Contains(x, y int) bool {
+func (this Recti) Contains(x, y int) bool {
 	return C.sfIntRect_contains(this.toCPtr(), C.int(x), C.int(y)) == 1
 }
 
-func (this *Rectf) Intersects(other *Rectf) (test bool, intersection *Rectf) {
+func (this Rectf) Intersects(other *Rectf) (test bool, intersection *Rectf) {
 	test = C.sfFloatRect_intersects(this.toCPtr(), other.toCPtr(), intersection.toCPtr()) == 1
 	return
 }
 
-func (this *Recti) Intersects(other *Recti) (test bool, intersection *Recti) {
+func (this Recti) Intersects(other *Recti) (test bool, intersection *Recti) {
 	test = C.sfIntRect_intersects(this.toCPtr(), other.toCPtr(), intersection.toCPtr()) == 1
 	return
 }
