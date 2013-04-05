698aae094a0512040136836a1203617e3398f3b5
paul <krepa098@gmail.com>
2013-04-05 20:26:45 +0200

    shader: use toCPtr(), fixed NewShaderFromMemory

--- shader.go
+++ shader.go
@@ -62,10 +62,20 @@ func NewShaderFromFile(vertexShaderFile, fragmentShaderFile string) (shader *Sha
 }
 
 func NewShaderFromMemory(vertexShader, fragmentShader string) (shader *Shader, err error) {
-	cVShader := C.CString(vertexShader)
-	cFShader := C.CString(fragmentShader)
-	defer C.free(unsafe.Pointer(cVShader))
-	defer C.free(unsafe.Pointer(cFShader))
+	var (
+		cVShader *C.char = nil
+		cFShader *C.char = nil
+	)
+
+	if vertexShader != "" {
+		cVShader = C.CString(vertexShader)
+		defer C.free(unsafe.Pointer(cVShader))
+	}
+
+	if fragmentShader != "" {
+		cFShader = C.CString(fragmentShader)
+		defer C.free(unsafe.Pointer(cFShader))
+	}
 
 	shader = &Shader{C.sfShader_createFromMemory(cVShader, cFShader)}
 	runtime.SetFinalizer(shader, (*Shader).Destroy)
@@ -79,7 +89,7 @@ func NewShaderFromMemory(vertexShader, fragmentShader string) (shader *Shader, e
 }
 
 func (this *Shader) Destroy() {
-	C.sfShader_destroy(this.cptr)
+	C.sfShader_destroy(this.toCPtr())
 	this.cptr = nil
 }
 
@@ -87,28 +97,28 @@ func (this *Shader) SetColorParameter(name string, color Color) {
 	cname := C.CString(name)
 	defer C.free(unsafe.Pointer(cname))
 
-	C.sfShader_setColorParameter(this.cptr, cname, color.toC())
+	C.sfShader_setColorParameter(this.toCPtr(), cname, color.toC())
 }
 
 func (this *Shader) SetTransformParameter(name string, trans Transform) {
 	cname := C.CString(name)
 	defer C.free(unsafe.Pointer(cname))
 
-	C.sfShader_setTransformParameter(this.cptr, cname, trans.toC())
+	C.sfShader_setTransformParameter(this.toCPtr(), cname, trans.toC())
 }
 
 func (this *Shader) SetTextureParameter(name string, texture *Texture) {
 	cname := C.CString(name)
 	defer C.free(unsafe.Pointer(cname))
 
-	C.sfShader_setTextureParameter(this.cptr, cname, texture.cptr)
+	C.sfShader_setTextureParameter(this.toCPtr(), cname, texture.cptr)
 }
 
 func (this *Shader) SetCurrentTextureParameter(name string) {
 	cname := C.CString(name)
 	defer C.free(unsafe.Pointer(cname))
 
-	C.sfShader_setCurrentTextureParameter(this.cptr, cname)
+	C.sfShader_setCurrentTextureParameter(this.toCPtr(), cname)
 }
 
 func (this *Shader) SetFloatParameter(name string, data ...float32) {
@@ -117,18 +127,20 @@ func (this *Shader) SetFloatParameter(name string, data ...float32) {
 
 	switch len(data) {
 	case 1:
-		C.sfShader_setFloatParameter(this.cptr, cname, C.float(data[0]))
+		C.sfShader_setFloatParameter(this.toCPtr(), cname, C.float(data[0]))
 	case 2:
-		C.sfShader_setFloat2Parameter(this.cptr, cname, C.float(data[0]), C.float(data[1]))
+		C.sfShader_setFloat2Parameter(this.toCPtr(), cname, C.float(data[0]), C.float(data[1]))
 	case 3:
-		C.sfShader_setFloat3Parameter(this.cptr, cname, C.float(data[0]), C.float(data[1]), C.float(data[2]))
+		C.sfShader_setFloat3Parameter(this.toCPtr(), cname, C.float(data[0]), C.float(data[1]), C.float(data[2]))
 	case 4:
-		C.sfShader_setFloat4Parameter(this.cptr, cname, C.float(data[0]), C.float(data[1]), C.float(data[2]), C.float(data[3]))
+		C.sfShader_setFloat4Parameter(this.toCPtr(), cname, C.float(data[0]), C.float(data[1]), C.float(data[2]), C.float(data[3]))
+	default:
+		panic("Shader.SetFloatParameter: Invalid amount of data.")
 	}
 }
 
 func (this *Shader) Bind() {
-	C.sfShader_bind(this.cptr)
+	C.sfShader_bind(this.toCPtr())
 }
 
 func ShaderAvailable() bool {
