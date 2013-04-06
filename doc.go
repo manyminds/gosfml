086ae4a8fba580a2b0ecf50c17ecc90eaa465aaf
paul <krepa098@gmail.com>
2013-04-06 14:23:31 +0200

    doc.go, formatting of the (c)Header

--- doc.go
+++ doc.go
@@ -8,22 +8,7 @@
 // 	2. 	Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 	3. 	This notice may not be removed or altered from any source distribution.
 
+/*
+Package gosfml2 provides access to the SFML2 API ( http://www.sfml-dev.org )
+*/
 package gosfml2
-
-// #include <SFML/System.h>
-import "C"
-
-/////////////////////////////////////
-///		WRAPPING HELPERS
-/////////////////////////////////////
-
-func sfBool2Go(b C.sfBool) bool {
-	return b == 1
-}
-
-func goBool2C(b bool) C.sfBool {
-	if b {
-		return C.sfBool(1)
-	}
-	return C.sfBool(0)
-}
