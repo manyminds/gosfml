5326a0bc03f0ae6c06645ca7b04109636dbdb003
paul <krepa098@gmail.com>
2013-04-20 10:14:59 +0200

    formatting

--- context.go
+++ context.go
@@ -4,9 +4,9 @@
 // Permission is granted to anyone to use this software for any purpose, including commercial applications, 
 // and to alter it and redistribute it freely, subject to the following restrictions:
 // 	1.	The origin of this software must not be misrepresented; you must not claim that you wrote the original software. 
-//		If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
-// 	2. 	Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
-// 	3. 	This notice may not be removed or altered from any source distribution.
+//			If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 	2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 	3. This notice may not be removed or altered from any source distribution.
 
 package gosfml2
 
@@ -28,7 +28,7 @@ type Context struct {
 
 // Create a new context
 //
-///This function activates the new context.
+// This function activates the new context.
 func NewContext() *Context {
 	context := &Context{C.sfContext_create()}
 	runtime.SetFinalizer(context, (*Context).destroy)
