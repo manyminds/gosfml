72f348fce88a3fa3bb362a3e93f9959fe0242c39
krepa098 <krepa098@gmail.com>
2014-03-11 10:52:14 +0100

    updated (C) headers

--- view.go
+++ view.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2012 by krepa098. All rights reserved.
+// Copyright (C) 2012-2014 by krepa098. All rights reserved.
 // Use of this source code is governed by a zlib-style
 // license that can be found in the license.txt file.
 
